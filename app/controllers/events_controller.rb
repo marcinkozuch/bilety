3fd5a339f8949e83034ba70fc2a343e4e006bf9a
Marcin Kożuch <marcinkozuch1994@gmail.com>
2019-01-21 23:32:57 +0100

    Add code to event view and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:komunikat] = 'Event został poprawnie stworzony.'
+      redirect_to "/events/#{@event.id}"
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:artist, :description, :price_low, :price_high, :event_date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/create'
+  root :to=> "pages#home"
+  resources :events, :only => [:index, :new, :create, :show]
   resources :tickets
-  root :to => "tickets#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
